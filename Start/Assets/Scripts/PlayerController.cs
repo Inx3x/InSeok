7c30b71761de643617d1a1fe9dc22757283439ab
Inx3x <skyuio300@gmail.com>
2021-08-24 09:08:23 +0900

    Unitychan

---
+++ Start/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    float _speed = 10.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Managers.Input.KeyAction -= OnKeyboard;
+        Managers.Input.KeyAction += OnKeyboard;
+    }
+
+    float _yAngle = 0.0f;
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnKeyboard()
+    {
+        _yAngle += Time.deltaTime * _speed;
+
+        //transform.eulerAngles = new Vector3(0.0f, Time.deltaTime * _speed, 0.0f);
+        // Quaternion qt = transform.rotation;
+        //transform.rotation = Quaternion.Euler(new Vector3(0.0f, _yAngle, 0.0f));
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.forward), 0.2f);
+            transform.position += (Vector3.forward * Time.deltaTime * _speed);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.back), 0.2f);
+            transform.position += (Vector3.back * Time.deltaTime * _speed);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.left), 0.2f);
+            transform.position += (Vector3.left * Time.deltaTime * _speed);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.right), 0.2f);
+            transform.position += (Vector3.right * Time.deltaTime * _speed);
+        }
+    }
+}
